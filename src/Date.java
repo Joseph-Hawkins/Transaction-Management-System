fe6907fff945986acf1b6b6bce490f22302c4f96
Gustavo <gustavoagarcia88@gmail.com>
2020-10-06 16:46:45 -0400

    Started off compare in Date

--- src/Date.java
+++ src/Date.java
@@ -3,6 +3,12 @@ public class Date implements Comparable<Date> {
 	private int month;
 	private int day;
 	
+	/**
+	 * Constructor class for date
+	 * @param year Year account was made
+	 * @param month Month account was made
+	 * @param day Day account was made
+	 */
 	public Date(int year, int month, int day) {
 		this.year = year;
 		this.month = month;
@@ -11,7 +17,9 @@ public class Date implements Comparable<Date> {
 
 	@Override
 	public int compareTo(Date o) {
-		// TODO Auto-generated method stub
+		if ( this.year > o.year ) {
+			return 0;
+		}
 		return 0;
 	}
 }
