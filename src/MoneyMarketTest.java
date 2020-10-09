2f1109b000662acd7f2238e597e4a01c81f64cd2
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-10-08 22:12:28 -0400

    Update MoneyMarketTest.java
    finishing

--- src/MoneyMarketTest.java
+++ src/MoneyMarketTest.java
@@ -25,8 +25,8 @@ class MoneyMarketTest {
 	}
 
 	@Test
-	void testMonthlyInterest() {
-		Profile john_cena = new Profile("John","Cena");
+    void testMonthlyInterest() {
+        Profile john_cena = new Profile("John","Cena");
         Date open_date = new Date(2018,10,9);;
         MoneyMarket item = new MoneyMarket(john_cena,2504.00,open_date);
         item.debit(1);
@@ -34,22 +34,18 @@ class MoneyMarketTest {
         item.debit(1);
         item.debit(1);
         
-        assertEquals(0,item.monthlyFee());
         assertEquals("1.35",String.format("%.2f", item.monthlyInterest()));
         item.debit(1000);
-        assertEquals(12,item.monthlyFee());
         assertEquals("0.81",String.format("%.2f", item.monthlyInterest()));
         item.debit(500);
-        assertEquals(12,item.monthlyFee());
         assertEquals("0.54",String.format("%.2f", item.monthlyInterest()));
-        item.credit(1000);
-        assertEquals(12,item.monthlyFee());
-        assertEquals("1.35",String.format("%.2f", item.monthlyInterest()));
-	}
+        item.credit(1600);
+        assertEquals("1.41",String.format("%.2f", item.monthlyInterest()));
+    }
 
-	@Test
-	void testMonthlyFee() {
-		Profile john_cena = new Profile("John","Cena");
+    @Test
+    void testMonthlyFee() {
+        Profile john_cena = new Profile("John","Cena");
         Date open_date = new Date(2018,10,9);;
         MoneyMarket item = new MoneyMarket(john_cena,2504.00,open_date);
         item.debit(1);
@@ -60,13 +56,10 @@ class MoneyMarketTest {
         assertEquals(0,item.monthlyFee());
         item.debit(1000);
         assertEquals(12,item.monthlyFee());
-        assertEquals(0.81,item.monthlyInterest());
         item.debit(500);
         assertEquals(12,item.monthlyFee());
-        assertEquals(0.54,item.monthlyInterest());
         item.credit(1000);
         assertEquals(12,item.monthlyFee());
-        assertEquals(1.35,item.monthlyInterest());
-	}
+    }
 
 }
